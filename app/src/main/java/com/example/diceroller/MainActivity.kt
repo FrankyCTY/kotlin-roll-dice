da4f94c14f3eaa31f300834eb361b5ff4555f043
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-08-29 02:09:29 +1200

    learn when statement

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,23 +1,49 @@
 package com.example.diceroller
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Roll dice on create
+        rollDice()
 
+        // on roll dice btn clicked
         val rollButton: Button = findViewById(R.id.rollBtn)
         rollButton.setOnClickListener {
-            val diceNumber = Dice(6).roll()
-
-            val rolledNumber: TextView = findViewById(R.id.rolledNum)
-            rolledNumber.text = diceNumber.toString()
+            rollDice()
         }
     }
+
+    // Roll dice and render dice image
+    private fun rollDice() {
+        val diceNumber = Dice(6).roll()
+
+        var rolledDiceImg = getDiceImageByRolledNumber(diceNumber)
+        val diceImage: ImageView = findViewById(R.id.diceImg)
+        diceImage.setImageResource(rolledDiceImg)
+        diceImage.contentDescription = diceNumber.toString()
+    }
+}
+
+
+fun getDiceImageByRolledNumber(rolledNumber: Int): Int {
+    val diceImage = when (rolledNumber) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    return diceImage
 }
 
 class Dice(private val numOfSides: Int) {
